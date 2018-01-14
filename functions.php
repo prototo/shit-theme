88f3d4c4995821b7cd2c555423e1d3f599ce13d6
Greg Mackelden <greg.mackelden@sony.com>
2018-01-14 02:33:01 +0000

    Adds conditional ga inclusion to functions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,9 @@
 <?php
 
+if (file_exists(__DIR__ . '/ga.php')) {
+    require_once(__DIR__ . '/ga.php');
+}
+
 if ( ! class_exists( 'Timber' ) ) {
 	add_action( 'admin_notices', function() {
 		echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php') ) . '</a></p></div>';
